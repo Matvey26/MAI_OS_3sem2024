67ef6c17bf61b0943d2106ed172b0cc9ea483d17
Matvey26 <matvey.ivchenko@yandex.ru>
2024-10-31 11:25:08 +0300

    101.5%

--- benchmark.sh
+++ benchmark.sh
@@ -5,17 +5,18 @@ gcc main.c -o main
 # Функция, чтобы замерять время исполнения программы для разного числа потоков
 function get_exec_time {
     local start_time=$(date +%s%3N)
-    ./main 100 100000 $1
+    ./main 1000000 10 $1
     local end_time=$(date +%s%3N)
     echo $((end_time-start_time)) 
 }
 
 # Замеряем время для двух потоков (для одного слишком быстро xD)
-T1=$(get_exec_time 2)
+T1=$(get_exec_time 1)
+echo "Количество потоков: 1, Время исполнения: $T1"
 
-for p in {3..50}; do
+for p in {2..50}; do
     Tp=$(get_exec_time $p)
     Sp=$(echo "scale=2; $T1 / $Tp" | bc)
     Xp=$(echo "scale=2; $Sp / $p" | bc)
-    echo "Количество потоков: $p, Ускорение: $Sp, Эффективность: $Xp" 
+    echo "Количество потоков: $p, Ускорение: $Sp, Эффективность: $Xp, Время исполнения: $Tp" 
 done

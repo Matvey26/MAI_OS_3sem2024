e2a9445b5b50bb5a295e51ea41be637594168448
Matvey26 <matvey.ivchenko@yandex.ru>
2024-12-22 15:56:47 +0300

    .

--- src/Client.hpp
+++ src/Client.hpp
@@ -177,7 +177,7 @@ private:
     void receive_messages() {
         while (running) {
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            try {я
+            try {
                 if (!response_connection or response_connection->empty()) continue;
                 std::string raw_message = response_connection->receive();
                 if (!raw_message.empty()) {
